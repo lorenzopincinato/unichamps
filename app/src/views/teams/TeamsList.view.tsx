e7792a06e3239df3e23f6a7ca22c385dd11190a0
Lorenzo Pincinato <lorenzopincinato@gmail.com>
2021-12-06 18:43:54 -0300

    Adiciona menu na tela de times

--- app/src/views/teams/TeamsList.view.tsx
+++ app/src/views/teams/TeamsList.view.tsx
@@ -5,6 +5,7 @@ import { MdAdd } from 'react-icons/md';
 
 import useTeamsList from 'src/hooks/teamsList.hook';
 import { TeamItem } from 'src/components/TeamItem.component';
+import ActionBar from 'src/components/ActionBar/ActionBar.component';
 
 const TeamsList: FC = () => {
   const {
@@ -23,32 +24,34 @@ const TeamsList: FC = () => {
   }, []);
 
   return (
-    <Container mt={20}>
-      <Container centerContent>
-        <Heading mt={3} color={'#276749'}>
-          Times
-        </Heading>
-      </Container>
-
-      <List spacing={3}>
-        {teams.map(team => {
-          return <TeamItem key={team.id} team={team} />;
-        })}
-      </List>
-
-      <Button
-        leftIcon={<MdAdd />}
-        colorScheme="green"
-        mt={4}
-        onClick={handleCreateTeam}
-      ></Button>
-
-      {error && (
-        <Container mt={4} centerContent>
-          {error ? error : 'Erro'}
+    <ActionBar>
+      <Container mt={20}>
+        <Container centerContent>
+          <Heading mt={3} color={'#276749'}>
+            Times
+          </Heading>
         </Container>
-      )}
-    </Container>
+
+        <List spacing={3}>
+          {teams.map(team => {
+            return <TeamItem key={team.id} team={team} />;
+          })}
+        </List>
+
+        <Button
+          leftIcon={<MdAdd />}
+          colorScheme="green"
+          mt={4}
+          onClick={handleCreateTeam}
+        ></Button>
+
+        {error && (
+          <Container mt={4} centerContent>
+            {error ? error : 'Erro'}
+          </Container>
+        )}
+      </Container>
+    </ActionBar>
   );
 };
 
